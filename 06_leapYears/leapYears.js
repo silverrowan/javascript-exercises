b7577767b43c8a470530797884c22a95708188e2
SilverRowan <silverrowan@gmail.com>
2025-04-03 15:55:19 -0700

    Complete Assn 06 Leap leapYears
    Note, not as easily readable as demo soln
    remember that variable names are easier to read
    than formulas and comments (even simple ones)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leap;
+	if(year % 4 === 0 && ( !(year % 100 === 0) || year % 400 === 0)) {
+        leap = true;
+    } else { leap = false; }
+	return leap;
 };
 
 // Do not edit below this line
