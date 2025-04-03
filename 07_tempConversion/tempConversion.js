e8ae0d29501c0fe8d58894f7e09abd318c677bc3
SilverRowan <silverrowan@gmail.com>
2025-04-03 16:19:48 -0700

    Complete Assn 07 Temperature tempConversion
    mine is a bit longer and more complicated with more
    functions, but clearer what is part of the tempConversion
    and what is rounding part of formulae.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function roundTo1Decimal(number) {
+	number = Math.round(number * 10) / 10;
+	return number;
+}
+
+const convertToCelsius = function(temperature) {
+  let convertedTemperature = roundTo1Decimal((temperature - 32 ) * (5/9));
+  return convertedTemperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+	let convertedTemperature = roundTo1Decimal(temperature * (9 / 5) + 32 );
+  return convertedTemperature;
 };
 
 // Do not edit below this line
