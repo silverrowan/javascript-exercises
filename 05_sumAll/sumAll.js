6d63d54fc916b72f89cbc56d4031db4cd75c4a03
SilverRowan <silverrowan@gmail.com>
2025-04-03 15:45:48 -0700

    Complete Assn 05* Sum All
    visually much longer code because of creating
    extra variables to switch values.
    
    Attempt by using the array deconstruction method
    as I believe it will be useful for personal project

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+	if (Number.isInteger(num1) && 
+		Number.isInteger(num2) && 
+		num1 >= 0 && 
+		num2 >= 0) {
+			let startNum;
+			let endNum;
+			let sumStep = 0;
 
-};
+			if (num1 <= num2) {
+				startNum = num1;
+				endNum = num2;
+			} else {
+				startNum = num2;
+				endNum = num1;
+			}
+
+			for (i = (startNum); i <= endNum; i++) {
+				sumStep += i;
+			}
+
+			return sumStep;
+	} else {
+		return "ERROR";
+	}
+}
 
 // Do not edit below this line
 module.exports = sumAll;
