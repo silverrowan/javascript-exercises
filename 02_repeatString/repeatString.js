441bef48fcfe3e1a09b233f7edda3d2d380462c2
SilverRowan <silverrowan@gmail.com>
2025-04-03 15:39:15 -0700

    Complete assn 02 Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringLine = "";
+    if (num < 0) {
+        stringLine = "ERROR";
+    } else {
+        for (i = 0; i < num; i++) {
+            stringLine += string;
+        }
+    }
+    return stringLine;
 };
 
 // Do not edit below this line
