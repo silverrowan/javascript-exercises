8dc52f27cf17034c5ab13bb996934f0daf17c60a
SilverRowan <silverrowan@gmail.com>
2025-04-03 15:43:49 -0700

    Complete Assn 04* Remove From Array
    splice method used is destructive
    attempt again using a filter method or by creating
    a new array and copying over.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+
+    for (j = 1; j < arguments.length; j++) {    
+
+        for (i = 0; i < array.length;) {
+            if (array[i] === arguments[j]) {
+		    	array.splice(i, 1);
+            } else {i++;}
+		}
+    }
+	return array;
 };
 
 // Do not edit below this line
