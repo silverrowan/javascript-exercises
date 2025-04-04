0dfbd39a722057298c8bce8c9062ed09b4901bd3
SilverRowan <silverrowan@gmail.com>
2025-04-03 20:38:40 -0700

    Complete Assign 08* calculator
    missed a significant number of array methods
    (and a few math ones) that become useful in this
    assignment, so things are done in wordier ways
    than needed.
    
    Redo after reading through the extended array and
    math methods.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
-};
+const add = function(num1, num2) {
+	let addNum = num1 + num2;
+	return addNum;	  
+  };
+  
+  const subtract = function(num1, num2) {
+	  let subtractNum = num1 - num2;
+	return subtractNum;
+  };
 
-const subtract = function() {
-	
-};
+  const sum = function(numArray) {
+	let arraySum = 0;
+	for (const num of numArray) {
+		arraySum += num;
+	}
+	return arraySum;
+  };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+  const multiply = function(numArray) {
+	let arrayMultiplied = 1;
+	for (const num of numArray) {
+		arrayMultiplied *= num;
+	}
+	return arrayMultiplied;
+  };
+  
+   const power = function(baseNum, exponentNum) {
+	  let numPower = 1;
+	  for (i = 0; i < exponentNum; i++) {
+		numPower *= baseNum;
+	  }
+	  return numPower;	  
+  };
+  
+  const factorial = function(num) {
+	let numFactoral = 1;
+	for (i = 0; i < (num); i++) {
+		numFactoral *= (num - i);
+	}
+	return numFactoral;
+  };
 
 // Do not edit below this line
 module.exports = {
