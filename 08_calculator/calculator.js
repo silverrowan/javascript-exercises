60855265aeebd84b26bcaf79677158de926a76f1
SilverRowan <silverrowan@gmail.com>
2025-04-10 00:07:13 -0700

    Create new formulas for calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
+const add = function(a, b) {
+    return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+    return a - b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    arraySum = array.reduce(addAll, 0);
+    return arraySum;	
 };
 
-const multiply = function() {
-
+function addAll (total, value) {
+    return total + value;
 };
 
-const power = function() {
-	
+const multiply = function(array) {
+    function multiplyAll (total, value) {
+        return total * value;
+    };
+    arrayMult = array.reduce(multiplyAll, 1);
+    return arrayMult;
 };
 
-const factorial = function() {
-	
+const power = function(a, b) {
+    return Math.pow(a,b);
+};
+
+const factorial = function(a) { // pretty sure last version was better
+    if (a === 0) {
+        return 1;
+    } else if (a === 1) {
+        return 1;
+    } else {
+        let i = a;
+        let numFactoral = i;
+        do {
+            numFactoral = numFactoral * (i - 1);
+            i = i - 1;
+        }
+        while (i > 1);
+        return numFactoral;
+    }
 };
 
 // Do not edit below this line
