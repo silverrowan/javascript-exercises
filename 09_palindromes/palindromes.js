e085fcc98e70b6d518fd1a41d9fb084f0f9731cf
SilverRowan <silverrowan@gmail.com>
2025-04-10 18:45:05 -0700

    Create code to check strings for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    stringModified = string.trim().toLowerCase().replaceAll((/\W/g || " "), "");
+    reverseString = Array.from(stringModified).toReversed().join("");      
+    return (reverseString == stringModified);
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// convert to an array (each letter -> 1 entry)
+// reverse the array
+// turn the array back into a string
+// return the string
+
