5ddb9f66b820e09d74ea526a1b3dfc2bf28fcc90
SilverRowan <silverrowan@gmail.com>
2025-04-10 22:21:17 -0700

    Create code* to get book titles from obj in array
    *Significant awkwardness -- partially from
    forgetting to add return line, partially from
    difficulties accessing object properties properly.
    
    Redo in a few days/after other projects to refresh

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookList) {
+    let titlesOnly = [];
+    for (let n in bookList) {
+        titlesOnly.push(bookList[n].title);
+    };
+    return titlesOnly;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+/*
+
+create object bookTitles by copying obj books
+    with a filter?
+
+For each array item
+    return array item title
+lol nope.
+*/
